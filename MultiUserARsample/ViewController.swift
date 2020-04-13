dccbc16ac53b474f5640a7c0cca5ab1f2d290600
stttr <zxcvbasdfgsr@gmail.com>
2020-04-13 16:35:42 +0900

    Initial Commit

---
+++ MultiUserARsample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultiUserARsample
+//
+//  Created by 佐藤涼輔 on 2020/04/13.
+//  Copyright © 2020 sato. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
